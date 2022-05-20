8ba99ad592d74729587cc7c6d281d81b3f4ce621
opto <willtucker110@gmail.com>
2022-05-20 15:46:59 +0000

    Removed test blocks

--- init.el
+++ init.el
@@ -1,5 +1,3 @@
-;; test 4
-
 (require 'package)
 (add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/"))
