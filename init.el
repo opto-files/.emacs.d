7adcd469177b28428c9fb9b65d1388d79be091e4
opto <willtucker110@gmail.com>
2022-05-23 00:10:43 +0000

    Added Paredit and some Eldoc stuff

--- init.el
+++ init.el
@@ -138,3 +138,30 @@
   :ensure t
   :config
     (setq inferior-lisp-program "sbcl"))
+
+(use-package paredit
+  :ensure t
+  :init  
+  :config
+    (add-hook 'emacs-lisp-mode-hook       #'enable-paredit-mode)
+    (add-hook 'eval-expression-minibuffer-setup-hook #'enable-paredit-mode)
+    (add-hook 'ielm-mode-hook             #'enable-paredit-mode)
+    (add-hook 'lisp-mode-hook             #'enable-paredit-mode)
+    (add-hook 'lisp-interaction-mode-hook #'enable-paredit-mode)
+    (add-hook 'scheme-mode-hook           #'enable-paredit-mode)
+    (add-hook 'slime-repl-mode-hook (lambda () (paredit-mode +1)))
+    ;; Stop SLIME's REPL from grabbing DEL,
+    ;; which is annoying when backspacing over a '('
+    (defun override-slime-repl-bindings-with-paredit ()
+      (define-key slime-repl-mode-map
+        (read-kbd-macro paredit-backward-delete-key) nil))
+    (add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit))
+
+(use-package eldoc
+  :ensure t
+  :config
+    (eldoc-add-command
+     'paredit-backward-delete
+     'paredit-close-round)
+     (add-hook 'emacs-lisp-mode-hook 'eldoc-mode)
+     (add-hook 'lisp-interaction-mode-hook 'eldoc-mode))
